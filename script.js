e262291be011ee12c373fee2f0efdd043845a1b8
Saksham Jha <sakshamjha3027@gmail.com>
2024-08-15 17:11:17 +0530

    script.js

--- script.js
+++ script.js
@@ -27,66 +27,73 @@ $(document).ready(function () {
     });
     // see more 2 script
     $(".seemore2").click(function () {
-      $(".seemore2").toggleCass("show");
+      $(".seemore2").toggleClass("show");
       $(".content2").toggleClass("show");
     });
     // see more 3 script
     $(".seemore3").click(function () {
-      $(".seemore3").togglClass("show");
+      $(".seemore3").toggleClass("show");
       $(".content3").toggleClass("show");
     });
     // see more 4 script
-    $(".seemore4").clickfunction () {
+    $(".seemore4").click(function () {
       $(".seemore4").toggleClass("show");
       $(".content4").toggleClass("show");
     });
     // see more 5 script
-    $(".seemore5").clickfunction () {
+    $(".seemore5").click(function () {
       $(".seemore5").toggleClass("show");
       $(".content5").toggleClass("show");
-    }
+    });
     // see more 6 script
     $(".seemore6").click(function () {
       $(".seemore6").toggleClass("show");
       $(".content6").toggleClass("show");
     });
     // see more 7 script
-    $(".seemore7").click(funon () {
-      $(".seemo7").toggleClass("show");
-      $(".content7").togglass("show");
+    $(".seemore7").click(function () {
+      $(".seemore7").toggleClass("show");
+      $(".content7").toggleClass("show");
     });
     // see more 7 script
     $(".seemore8").click(function () {
-      $(".seemore8")toggleClass("show");
+      $(".seemore8").toggleClass("show");
       $(".content8").toggleClass("show");
     });
     // read more script
     $(".readmore").click(function () {
-      $(".readmore").togleClass("show");
+      $(".readmore").toggleClass("show");
       $(".readmorecontent").toggleClass("show");
     });
     // typing animation script
     var typed = new Typed(".typing", {
       strings: [
-        "a Full Stack Web Developer",
+        "a Front End Web Developer",
         "Learning DSA",
         "Looking for new opportunities...",
       ],
-    
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true,
+    });
   
     var typed = new Typed(".typing-2", {
       strings: [
-        "a Full Stew opportunities...",
+        "a Front End Web Developer",
+        "Learning DSA",
+        "Looking for new opportunities...",
       ],
       typeSpeed: 100,
-      backSpeed
+      backSpeed: 60,
+      loop: true,
     });
     //owl carousel script
   
     $(".carousel").owlCarousel({
       margin: 20,
       loop: true,
-      autoploverPause: true,
+      autoplayTimeOut: 2000,
+      autoplayHoverPause: true,
       responsive: {
         0: {
           items: 1,
